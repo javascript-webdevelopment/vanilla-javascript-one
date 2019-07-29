afb4bdfa793b3627f6fd0c986ab506796a98ce05
tstokes9328 <taytestokes9328@gmail.com>
2019-07-28 21:32:59 -0600

    added property section

--- index.js
+++ index.js
@@ -12,8 +12,46 @@ function btnClick(){
     alert('The button has been clicked');
 };
 
+// .getElementById example
+const headerOne = document.getElementById('headerOne');
+console.dir(headerOne)
+
+
+
+// property examples
+const myList = document.getElementById('myList');
+
+// innerText
+console.log(myList.innerText)
+
+// innerHTML
+console.log(myList.innerHTML)
+
+// className
+const divOne = document.getElementById('divOne');
+console.log(divOne.className);
+
+// classList
+// Add A Class
+myList.classList.add('show');
+// Remove A Class
+myList.classList.remove('show');
+// Toggle A Class
+myList.classList.toggle('show');
 
 // .getElementByTagNames example
 const list = document.getElementById('myList');
-const numberOfLiTags = list.getElementsByTagName('li').length;
-console.log(numberOfLiTags)
+const numberOfLiTags = list.getElementsByTagName('li');
+console.dir(numberOfLiTags);
+
+// .getElemetByTagNames loop example
+for (let i = 0; i < numberOfLiTags.length; i++){
+    console.dir(numberOfLiTags[i])
+};
+
+// change color of every other element
+for (let i = 0; i < numberOfLiTags.length; i++){
+    if(i % 2 === 0){
+        numberOfLiTags[i].style.color = 'blue'
+    };
+};
